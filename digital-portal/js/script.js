9d057dab2dfeea6ea0fefb12b7587bb219156acb
ey-digital <clive.fernandes@in.ey.com>
2017-10-11 17:22:48 +0530

    v2.2.9

--- digital-portal/js/script.js
+++ digital-portal/js/script.js
@@ -78,6 +78,41 @@ $(function(){
 	}
 	
 	
+	if ( $().slick ) {
+	    $("#champions").slick({
+		  infinite: false,
+		  slidesToShow: 3,
+		  slidesToScroll: 3,
+		  responsive: [
+				{
+				  breakpoint: 1024,
+				  settings: {
+					slidesToShow: 3,
+					slidesToScroll: 3,
+				  }
+				},
+				{
+				  breakpoint: 600,
+				  settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2
+				  }
+				},
+				{
+				  breakpoint: 480,
+				  settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				  }
+				}
+			 ]		
+			
+			
+		});
+	}
+	
+	
+	
 	$("input[name='nature']").click(function () {
             if ($("#chargeable_sel").is(":checked")) {
                 $("#chargeable").show();
