5252d6840434c55b5b286749619709c4beb7a24f
ey-digital <clive.fernandes@in.ey.com>
2017-10-12 15:55:46 +0530

    v2.3.2

--- digital-portal/js/script.js
+++ digital-portal/js/script.js
@@ -132,6 +132,27 @@ $(document).on('change', '.file', function(){
   $(this).parent().find('.form-control').val($(this).val().replace(/C:\\fakepath\\/i, ''));
 });
 	
+	
+
+
+	
+/* Search overlay */
+	$('button[href="#search"]').on('click', function(event) {
+        event.preventDefault();
+        $('#search').addClass('open');
+        $('#search > form > input[type="search"]').focus();
+    });
+    
+    $('#search, #search button.close').on('click keyup', function(event) {
+        if (event.target == this || event.target.className == 'close' || event.keyCode == 27) {
+            $(this).removeClass('open');
+        }
+    });
+    
+    
+	
+	
+	
 // end bracket for document ready function 
 });
 
